27314caac7c44e765397ac689b4a2bcb71066395
danielrpg <daniel.fernandezgp@gmail.com>
2013-08-25 07:30:17 -0400

    Esta la prueba subida el sabado a la media noche

--- app/services.js
+++ app/services.js
@@ -1,6 +1,7 @@
 var url = "http://localhost:3000/";
 var codigo_operador;
 App.Services=(function(lng, app, undefined){
+	var socket = io.connect(url);
 	// Metodo que permite loguear en el sistema
 	var loginUsuario = function(login, password){
 		lng.Service.get(url+'loginOperador/'+login+'/'+password, {}, responseLogin, "json");
@@ -12,6 +13,20 @@ App.Services=(function(lng, app, undefined){
 			lng.Router.section("main-app-traker");
 			app.Geolocalization.initGeo();
 			codigo_operador = res.codigo_operador;
+			//console.log(socket);
+			socket.emit('nuevoUsuario', {login:$$('#login-usuario').val(), codigo_op:codigo_operador}, function(data){
+				if(data){
+					console.log('El usuario se ha añadido correctamente');
+				}else{
+					lng.Notification.error(
+					    "Error",                      //Title
+					    "El login de usuario ya esta siendo usado por otro",     //Description
+					    "remove",                     //Icon
+					    7,                            //Time on screen
+					    afterExecuteError             //Callback function
+					);
+				}
+			});
 		}else{
 			lng.Notification.error(
 			    "Error",                      //Title
@@ -30,6 +45,7 @@ App.Services=(function(lng, app, undefined){
     // Enviar los datos al servidor latitude y longitude
     var setCurrentPos = function(latitude, longitude){
     	lng.Service.post(url+"setCurrentPosition", {codigo_operador:codigo_operador,latitude:latitude, longitude:longitude}, successReponse, "json");
+    	socket.emit('compartirPoscion',{lat:latitude, lon:longitude, cod_op:codigo_operador});
     };
     var successReponse = function(response){
     	console.log(response);
