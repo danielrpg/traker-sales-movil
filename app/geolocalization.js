5724acebb0b0f2a9e619065087f421d71d8d5ec3
danielrpg <daniel.fernandezgp@gmail.com>
2013-08-28 21:31:20 -0400

    Habilitando el traker y el sensor

--- app/geolocalization.js
+++ app/geolocalization.js
@@ -2,12 +2,12 @@ App.Geolocalization = (function(lng, app, undefined){
 	var latitude;
 	var longitude;
 	var initGeolocalization = function(){
-		$$('#canvas_map').style("height","100%");
+		$$('#canvas_map').style("height","80%");
 		$$('#canvas_map').style("width","100%");
-		$$('#canvas_map').style("margin","0 auto");
 		if(navigator.geolocation){
-			initializa_map();
-			initialize();	
+			lng.Notification.show();
+			var options = {timeout: 10000, maximumAge: 11000, enableHighAccuracy: true };
+			navigator.geolocation.watchPosition(showMap, errorMap, options);
 		}else{
 			lng.Notification.error(
 			    "Error",                      //Title
@@ -18,37 +18,7 @@ App.Geolocalization = (function(lng, app, undefined){
 			);
 		}
 	};
-	var initializa_map = function(){
-		var myOptions = {
-	      zoom: 4,
-	      mapTypeControl: true,
-	      mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
-	      navigationControl: true,
-	      navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
-	      mapTypeId: google.maps.MapTypeId.ROADMAP      
-	    };
-	    map = new google.maps.Map(document.getElementById("canvas_map"), myOptions);	
-	};
 
-	var initialize = function(){
-		if(geo_position_js.init())
-		{
-			//document.getElementById('current').innerHTML="Receiving...";
-			lng.Notification.show();
-			geo_position_js.getCurrentPosition(showMap,errorMap,{enableHighAccuracy:true});
-		}
-		else
-		{
-			//document.getElementById('current').innerHTML="Functionality not available";
-			lng.Notification.error(
-			    "Error",                      //Title
-			    "No soporta la geolocalizacion por favor instalar un navegador que soporte.",     //Description
-			    "remove",                     //Icon
-			    7,                            //Time on screen
-			    afterExecuteError             //Callback function
-			);
-		}
-	};
 	var afterExecuteError = function(){
 		//document.getElementById('current').innerHTML="Couldn't get location"
 	};
@@ -58,26 +28,21 @@ App.Geolocalization = (function(lng, app, undefined){
 		console.log(latitude+","+longitude);
 		app.Services.setCurrentPos(latitude,longitude);
 		var pos=new google.maps.LatLng(latitude, longitude);
-		map.setCenter(pos);
-		map.setZoom(16);
-		var infowindow = new google.maps.InfoWindow({
-		    content: "<strong>yes</strong>"
-		});
-
-		var marker = new google.maps.Marker({
-		    position: pos,
-		    map: map,
-		    title:"You are here"
-		});
-
-		google.maps.event.addListener(marker, 'click', function() {
-		  infowindow.open(map,marker);
-		});
+		var opciones = {
+			zoom:16,
+			center:pos,
+			mapTypeId:google.maps.MapTypeId.ROADMAP
+		};
+		var mapa = new google.maps.Map(document.getElementById('canvas_map'), opciones);
+		var opcMark = {
+			position : pos,
+			map : mapa,
+			title : 'Coordenada de Casa'
+		};
+		var mak = new google.maps.Marker(opcMark);
 		lng.Notification.hide();
 	};
 	var errorMap = function(){
-		//console.log();
-	//	document.getElementById('current').innerHTML="Couldn't get location";
 		lng.Notification.error(
 		    "Error",                      //Title
 		    "No se puede optener la localizacion.",     //Description
